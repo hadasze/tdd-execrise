970de983188548a3e5bbc0d54ce18629624ae0f6
Yakov Haiilo <yakovh@wix.com>
2021-02-07 20:44:14 +0200

    tic tac toe TDD.

--- exercises/client-tdd/src/components/App/App.js
+++ exercises/client-tdd/src/components/App/App.js
@@ -24,17 +24,30 @@ class App extends React.Component {
     winner: '',
   };
 
+
   handleCellClick = (rowIndex, colIndex) => {
     const board = _.cloneDeep(this.state.board);
-    board[rowIndex][colIndex] = this.state.currentPlayer;
-    const nextPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X';
+    let nextPlayer = this.state.currentPlayer;
+    if(board[rowIndex][colIndex] === ''){
+      board[rowIndex][colIndex] = this.state.currentPlayer;
+      nextPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X';
+    }
     const winner = getWinner(board);
     this.setState({ board, currentPlayer: nextPlayer, winner });
   };
 
+  getWinnerName = () => {
+    const { winner,player1,player2 } = this.state;
+    if( winner && winner !== 'Tie'){
+      if(winner === 'X'){
+        return player1;
+      }
+      return player2;
+    }
+  };
+
   render() {
     const { t } = this.props;
-
     return (
       <div className={s.root}>
         <h2 className={s.title} data-testid="app-title">
@@ -48,12 +61,12 @@ class App extends React.Component {
         />
 
         <h3>
-          <span>Game is on!</span>
-          <span id="player-1-title">
+          <span>Game is on! </span>
+          <span id="player-1-title" className={this.state.currentPlayer === 'X' ? s.bold : null}>
             {this.state.isGameStarted && this.state.player1}
           </span>
-          <span>VS.</span>
-          <span id="player-2-title">
+          <span> VS. </span>
+          <span id="player-2-title" className={this.state.currentPlayer === 'O' ? s.bold : null}>
             {this.state.isGameStarted && this.state.player2}
           </span>
         </h3>
@@ -69,7 +82,7 @@ class App extends React.Component {
                       className={s.cell}
                       key={key}
                       data-hook={key}
-                      onClick={() => this.handleCellClick(rowIndex, colIndex)}
+                      onClick={() => this.state.isGameStarted && this.handleCellClick(rowIndex, colIndex)}
                     >
                       {cellValue}
                     </span>
@@ -80,9 +93,7 @@ class App extends React.Component {
           })}
         </div>
 
-        <h3>
-          The winner is: <span id="winner">{this.state.winner}</span>
-        </h3>
+        <h3>The winner is: <span id="winner">{this.getWinnerName()}</span></h3>
       </div>
     );
   }
